bb80baed9993cc53918928a1aa32211aaafe67bb
Muhammadjon <muhammadjo15487@mail.ru>
2025-05-07 22:40:44 +0500

    Noting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // правильный импорт — это CSS, а не HTML
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
